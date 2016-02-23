ee31cce22c04601a7aeb4ff39cfd21eef1022443
Benny Paulino <santiagopro71@gmail.com>
2016-02-23 21:40:34 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pajaro::Application.config.secret_key_base = '378afd80dea8feb7cd1bd47730392d1b653e3567aa825f87e9bf46dd1040a6e4cd53d158d11d5a428e346001abc31fe28baade3d4e5381e870c8db4c9b116d0f'
